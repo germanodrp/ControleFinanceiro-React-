adea34b39326758e44b4d9e239436291f221dad2
Germano Pereira <germano.pereira@bit.com.br>
2022-07-12 13:19:12 -0300

    começo projeto

---
+++ projeto/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Header from "./Components/Header";
+import GlobalStyles from './Styles/global'
+
+const App = () => {
+    return(
+        <>
+    <Header/>
+    <GlobalStyles/>
+    </>
+    );
+};
+
+export default App;
