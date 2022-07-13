973fa3cf4c6279888722a77314d5511e679f5472
Germano Pereira <germano.pereira@bit.com.br>
2022-07-13 13:42:53 -0300

    feat:Finalização de App

--- projeto/src/Components/Header/styles.js
+++ projeto/src/Components/Header/styles.js
@@ -2,7 +2,7 @@ import styled from "styled-components";
 export const Container = styled.div`
 height:150px;
 text-align:center;
-background:teal;
+background:#d02941;
 `;
 
 export const Header = styled.h1``;
